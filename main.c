c3bc733595b373fb233ea0b0afe0b184d83c597a
Pongpan P <44573264+pongpanp@users.noreply.github.com>
2025-01-10 16:31:29 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int isSuccess = signIn();
+	int isExit;
+	if (isSuccess) {
+		do{
+			isExit = mainMenu();
+		} while (isExit != 0);
+	}
+	return 0;
+}
+
+int signIn() {
+	int pwMaster = 12345;
+	int pwInput;
+	
+	int loopCheck = 1;
+	
+	do {
+		printf("Input Password ==> ");
+		scanf("%d", &pwInput);
+		
+		if(pwInput == pwMaster) {
+		return 1;
+		}
+		
+		(loopCheck != 4) 
+			? printf("Input password at %d no correct\n", loopCheck) 
+			: printf("!!! Exit !!!\n", loopCheck);
+		printf("...............................\n");
+				
+		loopCheck++;
+	} while (loopCheck <= 4);
+	return 0;
+}
+
+int mainMenu() {
+	int choice;
+	
+	printf("...............................\n");
+	printf("         Main Program\n");
+	printf("...............................\n");
+	printf(" 1. +\n 2. -\n 3. x\n 4. div\n 5. mod\n 6. Exit\n");
+	printf("...............................\n");
+	printf("Select to choice ==> ");
+	
+	scanf("%d", &choice);
+	printf("...............................\n");
+	
+	if (choice != 6) {
+		calculator(choice);
+		return 1;
+	}
+	
+	printf("<< Bye Bye >>");
+	return 0;
+}
+
+int calculator(int choice) {
+	int inputNumber;
+	int number1;
+	int number2;
+		
+	
+	printf("Input Number 1 [1-10] ==> ");
+	scanf("%d/n", &inputNumber);
+	number1 = inputNumber;
+	
+	
+	printf("Input Number 2 [1-10] ==> ");
+	scanf("%d/n", &inputNumber);	
+	
+	if (choice != 1 && inputNumber > number1)  {
+	number2 = number1;
+	number1 = inputNumber;	
+	} else {
+		number2 = inputNumber; 
+	}
+
+	
+	
+	printf("...............................\n");
+	
+
+	switch (choice) {
+		case 1: 
+			printf("%d + %d = %d \n", number1,  number2 , number1 + number2);
+			return 1;
+		break;
+		case 2: 
+			printf("%d - %d = %d \n", number1,  number2 , number1 - number2);
+			return 1;
+		break;
+		case 3: 
+			printf("%d x %d = %d \n", number1,  number2 , number1 * number2);
+			return 1;
+		break;
+		case 4: 
+			printf("%d div %d = %d \n", number1,  number2 , number1 / number2);
+			return 1;
+		break;
+		case 5: 
+			printf("%d mod %d = %d \n", number1,  number2 , number1 % number2);
+			return 1;
+		default: return 1;
+	}
+
+	return 1;
+}
